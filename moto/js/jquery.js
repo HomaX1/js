70ea9b579784ee49261ebde9ff9861b2af85d70f
HomaX1 <baitasova.anastasiia@gmail.com>
2016-11-30 23:16:04 +0200

    unit 2

--- moto/js/jquery.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by admin on 28.11.2016.
- */
-/* //ДЗ №1
-$('#my_links a[href ^= documents]');
-$('#forfooter img[title = Производители]');*/
-
--- moto/js/myscripts.js
+++ moto/js/myscripts.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
 
+//$('img[src*=logo]').fadeOut(4000).fadeIn(4000); //плавное исчезновение и появление
+
+    function hidden (element, time) {
+        var idElem = "#" + element;
+
+        if(typeof time == "number" || time > 500 || time < 10000) {
+
+            $(idElem).fadeOut(time);
+        }else{
+            return false;
+        }
+    }
+
+    hidden('moto_models', 3000);
 
 
-
-}); //означает, что все скрипты будут выполнятся после готовности всей страницы, короткая версия $(function() {});
+});//означает, что все скрипты будут выполнятся после готовности всей страницы, короткая версия $(function() {});
 //Конец ready
 
 
