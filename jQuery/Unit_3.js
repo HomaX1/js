31942bd3818fe6100c42f073b37e44e53d0d37a1
HomaX1 <baitasova.anastasiia@gmail.com>
2016-12-09 16:32:17 +0200

    continue unit 3

--- jQuery/Unit_3.js
+++ jQuery/Unit_3.js
@@ -1,4 +1,4 @@
-//СОБЫТИЯ МЫШИ, КОТОРЫЕ ОТСЛЕЖИВАЮТСЯ БРАУЗЕРОМ
+                    //СОБЫТИЯ МЫШИ, КОТОРЫЕ ОТСЛЕЖИВАЮТСЯ БРАУЗЕРОМ
 
 
 mouseover // событие, когда пользователь НАВОДИТ на елемент
@@ -11,7 +11,7 @@ mouseup //отпускаете мышь после клика
 
 
 
-//СОБЫТИЯ С ФОРМАМИ
+                    //СОБЫТИЯ С ФОРМАМИ
 
 
 submit //отправка формы
@@ -25,10 +25,132 @@ keyup //опускаем клавишу после нажатия
 
 
 
-//СОБЫТИЯ ОКНА БРАУЗЕРА
+                    //СОБЫТИЯ ОКНА БРАУЗЕРА
 
 
 load // ксобытие, когда брайзер запустил и все загружено с сервера
 resize //меняется ширина окна браузера
 scroll //прокрутка страницы с помощью мыши или сбоку в браузере
 unload //выгрузка, когда закрываем страницы или переходим на другую страницу
+
+
+
+// накладываем событие на объект либо в html коде как onclick="alert('тест')" или в js
+
+
+ var myMoto = document.getElementById('img_1'); //объект
+ function motoClick() { //действие
+ alert("Ширина данной картинки: " + myMoto.width);
+ }
+ myMoto.onclick = motoClick(); //событие, к событию добавляется приставка on
+
+
+
+                    //jQuery модель событий
+
+
+$('#forheader img').click(function() {
+    alert('Вы кликнули по шапке');
+});
+
+
+$(window).scroll(function() {
+    alert('Произошло событие прокрутки');
+});
+
+
+
+                    //Кликаем на мото и он же появляется в другом месте
+
+
+$('#div_for_img img').click(function() {
+    var moto = $(this).clone();
+    $('#forresults').append(moto);
+});
+
+
+
+                    //ДЗ 1
+
+                    $('#moto_models li').click(function() {
+                        var myText = $(this).text();
+                        $('#moto_table tbody').append('<tr><td>' + myText + '</td><td>25000</td></tr>');
+                    });
+
+
+
+                    //ДЗ 2
+
+                    var tableStr = $('#moto_table tr');
+
+                    tableStr.mouseover(function() {
+                        $(this).addClass('light');
+                    });
+
+                    tableStr.mouseout(function() {
+                        $(this).removeClass('light');
+                    });
+
+
+
+
+                    //jQuery собитий hover() и toggle()
+
+                    $('#moto_table tr').hover(function() { //при наведении действует первая ф-ция, при увода мыши - вторая
+                        $(this).addClass('light');
+                    }, function() {
+                        $(this).removeClass('light');
+                    });
+
+
+                    $('#formHide').toggle(function() { // при клике на текст
+                        $('#my_form').fadeOut(1000);
+                        $(this).text('Развернуть форму');
+                    }, function() {
+                        $('#my_form').fadeIn(1000);
+                        $(this).text('Свернуть форму');
+                    });
+
+
+
+                    //ДЗ 3
+
+                    $('#my_button').hover(function() {
+                        $(this).css({'background-color': '#32375D', 'color': '#ffffff'});
+                    }, function() {
+                        $(this).css({'background-color': '#f6f6f6', 'color': '#2C2C2C'});
+                    });
+
+
+                    $('#moto_table tbody tr').toggle(function() {
+                        $(this).addClass('forTr');
+                    }, function() {
+                        $(this).removeClass('forTr');
+                    });
+
+
+
+
+                    //Понятие объекта события
+
+                    $('#div_for_img').click(function(eventObject) {//или (е) // узнать в каком месте экрана монитора произошел щелчек
+                        alert('Координата щелчка по оси Х: ' + eventObject.screenX);
+                    });
+
+                    // pageX, pageY - место щелчка в браузере
+                    // altKey - (true/false) была ли нажата клавиша alt в момент щелчка
+                    // ctrlKey - (true/false) была ли нажата клавиша ctrl в момент щелчка
+                    // shiftKey - (true/false) была ли нажата клавиша shift в момент щелчка
+
+
+                    // target - какой объект отреагировал на событие
+
+                    $('#div_for_img').click(function(eventObject) {
+                        var myObject = eventObject.target;
+                        alert('Объект отреагировавший на клик ' + myObject.id); //выдает ИД картинки к примеру
+                    });
+
+
+
+                    //ДЗ 4
+
--- moto/js/myscripts.js
+++ moto/js/myscripts.js
@@ -2,6 +2,39 @@ $(document).ready(function() {
 
 
 
+   /* $('#div_for_img').click(function(eventObject) {//или (е) // узнать в каком месте экрана монитора произошел щелчек
+        alert('Координата щелчка по оси Х: ' + eventObject.screenX);
+    });
+
+    // pageX, pageY - место щелчка в браузере
+    // altKey - (true/false) была ли нажата клавиша alt в момент щелчка
+    // ctrlKey - (true/false) была ли нажата клавиша ctrl в момент щелчка
+    // shiftKey - (true/false) была ли нажата клавиша shift в момент щелчка
+
+
+    // target - какой объект отреагировал на событие
+
+    $('#div_for_img').click(function(eventObject) {
+        var myObject = eventObject.target;
+        alert('Объект отреагировавший на клик ' + myObject.id); //выдает ИД картинки к примеру
+    });
+*/
+
+    $('#div_for_img img').toggle(function(eventObject) {
+
+        if(eventObject.shiftKey) {
+            $(this).css('border', '4px solid #cc0000');
+        }
+        else {
+            $(this).css('border', '4px solid #333333');
+        }
+
+    }, function() {
+        $(this).css('border', 'none');
+    });
+
+
+
 
 
 
